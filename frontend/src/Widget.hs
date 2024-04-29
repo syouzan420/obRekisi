d0f7d1d6e04ae312126dc033327aabb06eda6387
syouzan420 <syouzanponkikki@yahoo.co.jp>
2024-04-29 13:27:17 +0900

    update

--- frontend/src/Widget.hs
+++ frontend/src/Widget.hs
@@ -11,9 +11,9 @@ import EReki (Rdt(..), reki, sortNens)
 import Reflex.Dom.Core 
   ( text, dynText, el, elAttr, divClass, elAttr', blank
   , (=:), leftmost, accumDyn, elDynAttr, prerender 
-  , holdDyn, domEvent, zipDyn, zipDynWith, current, gate
+  , holdDyn, domEvent, zipDynWith, current, gate
   , tickLossyFromPostBuildTime, widgetHold_
---  , tag
+  , tag
   , DomBuilder, Prerender, PerformEvent, TriggerEvent
   , PostBuild, Event, EventName(Click), MonadHold ,Dynamic
   , Performable, TickInfo(..)
@@ -43,7 +43,6 @@ elButtonMondai = do
   el "p" $ text ""
   widgetHold_ elButtonAction tb 
 
-
 buttonClass :: DomBuilder t m => T.Text -> T.Text -> m (Event t ())
 buttonClass c s = do
   (e, _) <- elAttr' "button" ("type" =: "button" <> "class" =: c) $ text s
@@ -70,30 +69,20 @@ elButtonAction ::
 elButtonAction = do
   rec
     (dyAns,dyRdt) <- elMondai
-    let dyMon = fmap makeMon dyRdt
-    let dyHnt = fmap makeHnt dyRdt
-    let dyZipMH = zipDyn dyMon dyHnt
     dyTime <- elCharaAnime (fmap not dyIsAnsCorrect)
     let dyIsTime60 = fmap (\ti -> (ti/="start") && (read . T.unpack) ti>(60::Int)) dyTime
-    let dyMH = zipDynWith (\bl (m,h)-> if bl then h else m) dyIsTime60 dyZipMH 
-    el "p" $ text ""
-    divClass "kai" (dynText dyMH)
-    el "p" $ text ""
+    elSpace
+    mapM_ (\n -> makeHMon dyIsTime60 (n+1) (fmap (\rdts -> if null rdts then Rdt 0 T.empty T.empty T.empty else rdts!!n) dyRdt)) [0..(qNum-1)]
+    elSpace
     numberButton <- numberPad qNum 
     clearButton <- buttonClass "pad" "B"
     let buttons = leftmost [ ButtonClear <$ clearButton
                            , ButtonNumber <$> numberButton
                            ]
     dyState <- accumDyn collectButtonPresses initialState buttons
---  let dyKoto = fmap makeKoto dyRdt
-    let dyCont = fmap makeCont dyRdt
     let dyIsAnsCorrect = zipDynWith (==) dyAns dyState
-    let dyRes = zipDynWith (\a b->if a==b then "せいかい!" else T.empty) dyAns dyState 
-        dyRes2 = zipDynWith (\a b-> if a/=T.empty then b else T.empty) dyRes dyCont
     el "p" $ dynText dyState
-    el "p" $ dynText dyRes
-    divClass "kai" $ dynText dyRes2
---    mapM_ (\n -> makeKai (fmap (\rdts -> if null rdts then Rdt 0 T.empty T.empty T.empty else rdts!!n) dyRdt)) [0..(qNum-1)]
+    elKai dyIsAnsCorrect dyRdt
   pure ()
   where
     initialState :: T.Text
@@ -105,21 +94,20 @@ elButtonAction = do
         ButtonClear -> if state==T.empty then T.empty else T.init state 
         ButtonNumber digit -> state <> digit
 
-{-
 elKai ::
   ( DomBuilder t m
-  , MonadFix m
   , MonadHold t m
   , PostBuild t m
-  ) => Dynamic t T.Text -> Dynamic t T.Text -> m ()
-elKai dyKoto dyCont = do
-  el "div" $ text "せいかい!"
-  evMore <- buttonClass "text" "more"
-  dyToggle <- toggle True evMore 
-  let dyZipKC = zipDyn dyKoto dyCont
-  let dyKC = zipDynWith (\bl (k,c) -> if bl then k else c) dyToggle dyZipKC
-  divClass "kai" $ dynText dyKC 
--}
+  ) => Dynamic t Bool -> Dynamic t [Rdt] -> m ()
+elKai dyIsAnsCorrect dyRdt = do
+  let dHide = mkHidden <$> dyIsAnsCorrect
+  let dySRdt = fmap makeSort dyRdt
+  elDynAttr "div" dHide $ do 
+    el "p" $ text "せいかい!"
+    elSpace
+    mapM_ (\n -> makeKai (fmap (\rdts -> if null rdts then Rdt 0 T.empty T.empty T.empty else rdts!!n) dySRdt)) [0..(qNum-1)]
+  where mkHidden False = "hidden" =: "" 
+        mkHidden True = mempty
 
 elMondai :: 
   ( DomBuilder t m
@@ -134,27 +122,31 @@ elMondai = do
 makeAns :: ([Rdt],[Int]) -> T.Text
 makeAns (_,ils) = (T.pack . concatMap show) ils
 
-makeMon :: [Rdt] -> T.Text
-makeMon rdt = T.intercalate "\n" $
-       zipWith (\i (Rdt _ k _ _) -> (T.pack . show) i <> ": " <> k) [1::Int,2..] rdt
+makeSort :: [Rdt] -> [Rdt]
+makeSort rdt = map (\(n,(k,h,c)) -> Rdt n k h c) $
+                  sortNens $ map (\(Rdt n k h c) -> (n,(k,h,c))) rdt 
 
-makeHnt :: [Rdt] -> T.Text
-makeHnt rdt = T.intercalate "\n" $
-       zipWith (\i (Rdt _ k h _) -> (T.pack . show) i <> ": " <> k <> "\n  ----" <> h)
-                                                                    [1::Int,2..] rdt
-{--
-makeKoto :: [Rdt] -> T.Text
-makeKoto rdt = T.intercalate "\n" $
-              map (\(n,k) -> (T.pack . show) n <> "年: " <> k) $
-              sortNens $ map (\(Rdt n k _ _) -> (n,k)) rdt 
---}
+makeHMon :: 
+  ( DomBuilder t m
+  , MonadHold t m
+  , PostBuild t m
+  )  => Dynamic t Bool -> Int -> Dynamic t Rdt -> m ()
+makeHMon dyIsTime60 i dyRdt = do
+  divClass "kai" $ do 
+    let dHide = mkHidden <$> dyIsTime60
+    let dyMon = fmap (\(Rdt _ k _ _) -> 
+                      (T.pack . show) i <> ": " <> k) dyRdt 
+    dynText dyMon
+    elDynAttr "div" dHide $ do 
+      let dyH = fmap (\(Rdt _ _ h _) -> "-----"<>h) dyRdt
+      let beH = current dyH
+      evB <- buttonClass "text" "hint"
+      let evH = tag beH evB
+      dynText =<< holdDyn T.empty evH 
+      blank
+  where mkHidden False = "hidden" =: "" 
+        mkHidden True = mempty 
 
-makeCont :: [Rdt] -> T.Text
-makeCont rdt =  T.intercalate "\n" $
-           map (\(n,(k,c)) -> (T.pack . show) n <> "年: " <> k <> "\n    " <> c) $
-           sortNens $ map (\(Rdt n k _ c) -> (n,(k,c))) rdt 
-
-{--
 makeKai :: 
   ( DomBuilder t m
   , MonadHold t m
@@ -164,14 +156,12 @@ makeKai dyRdt = do
   divClass "kai" $ do 
     dynText $ fmap (\(Rdt n k _ _) -> (T.pack . show) n <> "年: " <> k) dyRdt
     text "  "
-    let dyC = fmap (\(Rdt _ _ _ c) -> "\n"<>c) dyRdt
+    let dyC = fmap (\(Rdt _ _ _ c) -> "\n-----"<>c<>"\n ") dyRdt
     let beC = current dyC
     evB <- buttonClass "text" "more"
     let evC = tag beC evB
     dynText =<< holdDyn T.empty evC 
     blank
---}
-
 
 elTimer :: 
   ( DomBuilder t m
@@ -212,7 +202,6 @@ elCharaAnime isAnsNotCorrect = do
   elDynAttr "div" dHide1 $ do elChara0; dynText dyTime 
   elDynAttr "div" dHide2 $ do elChara1; dynText dyTime 
   pure dyTime 
-    
   
 elChara :: DomBuilder t m => m ()
 elChara = elAttr "img" ("src" =: $(static "chara0_mid.png")) blank
